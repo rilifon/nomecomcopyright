864a64ced10e1a218e3043aee2b0c7e71f981626
Gabriel Massukado <gabriel.massukado@usp.br>
2021-08-03 18:59:54 -0300

    foto do cachorro e codigo inutil

--- project/game/Slider.gd
+++ project/game/Slider.gd
@@ -1,2 +1,18 @@
 extends Node2D
 
+const MAX_ROWS : int = 20
+const MAX_COLUMNS : int = 20
+const WIDTH : int = 1024
+const HEIGHT : int = 1024
+
+export var rows : int
+export var columns : int
+
+var picture : Resource
+
+func _ready() -> void:
+	print("atchim")
+	picture = get_picture()
+
+func get_picture() -> void:
+	pass
