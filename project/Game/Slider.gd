d3c431f91dac4544bd6aa1fb1a724bda0b356d1f
Gabriel Massukado <gabriel.massukado@usp.br>
2021-08-10 18:44:39 -0300

    mais coisas nao testadas yay

--- project/Game/Slider.gd
+++ project/Game/Slider.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const PIECE = preload("res://Game/SliderPiece.tscn")
+
 const MAX_ROWS : int = 20
 const MAX_COLUMNS : int = 20
 const WIDTH : int = 1024
@@ -8,16 +10,27 @@ const HEIGHT : int = 1024
 export var rows : int
 export var columns : int
 
+var board : Array
 var picture : ImageTexture
 
 func _ready() -> void:
 	print("atchim")
 	picture = get_picture()
+	make_board()
+
 
 func get_picture(path : = "image.jpg") -> ImageTexture:
 	var img = Image.new()
 	var texture = ImageTexture.new()
 	img.load("res://Assets/" + path)
-	img.resize(WIDTH, HEIGHT)
+	img.resize(WIDTH, HEIGHT) # essa linha vai ser trocada por algo que funciona uwu
 	texture.create_from_image(img)
 	return texture
+
+
+func make_board() -> void:
+	for r in range(rows):
+		for c in range(columns):
+			var new_piece = PIECE.instance()
+			board[r][c] = new_piece
+			add_child(new_piece)
