95672d55fbdad3cf6d912f123b7b87820e9af307
Débora Prudêncio <debora.prudencio@gmail.com>
2021-10-28 19:13:22 -0300

    add return home button and reposition timer

--- project/Game/IngameSettings.gd
+++ project/Game/IngameSettings.gd
@@ -20,3 +20,10 @@ func _on_MovesToggle_toggled(button_pressed):
 
 func _on_Close_pressed():
 	hide()
+
+
+func _on_ReturnHome_pressed():
+	var screen = load("res://Game/ImgSelectScreen.tscn").instance()
+	
+	get_tree().get_root().add_child(screen)
+	owner.queue_free()
